1da76153ab9f085325d4542900c3abe08cb09f4e
ohenecoker <coker.christian@proton.me>
2023-05-28 10:39:52 -0400

    add all code from tutorial

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,26 +16,186 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+
+async function initStream() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+      width: "1280",
+    });
+    document.getElementById("local-video").srcObject = stream;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+let Hooks = {};
+Hooks.JoinCall = {
+  mounted() {
+    initStream();
+  },
+};
+
+Hooks.InitUser = {
+  mounted() {
+    addUsersConnection(this.el.dataset.userUuid);
+  },
+  destroyed() {
+    removeUserConnection(this.el.dataset.userUuid);
+  },
+};
+
+var users = {};
+
+function addUsersConnection(userUuid) {
+  if (users[userUuid] === undefined) {
+    users[userUuid] = {
+      peerConnection: null,
+    };
+  }
+  return users;
+}
+
+function removeUserConnection(userUuid) {
+  delete users[userUuid];
+  return users;
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
+  params: { _csrf_token: csrfToken },
+});
+
+function createPeerConnection(lv, fromUser, offer) {
+  let newPeerConnection = new RTCPeerConnection({
+    iceServers: [{ urls: "stun:littlechat.app:3478" }],
+  });
+  console.log("users: ",users)
+
+  users[fromUser].peerConnection = newPeerConnection;
+
+  localStream
+    .getTracks()
+    .forEach((track) => newPeerConnection.addTrack(track, localStream));
+
+  if (offer !== undefined) {
+    newPeerConnection.setRemoteDescription({ type: "offer", sdp: offer });
+    newPeerConnection
+      .createAnswer()
+      .then((answer) => {
+        newPeerConnection.setLocalDescription(answer);
+        console.log("Sending this ANSWER to the requester:", answer);
+        lv.pushEvent("new_answer", {
+          toUser: fromUser,
+          description: description,
+        });
+      })
+      .catch((err) => console.log(err));
+
+    newPeerConnection.onicecandidate = async ({ candidate }) => {
+      lv.pushEvent("new_ice_candidate", { toUser: fromUser, candidate });
+    };
+
+    if (offer === undefined) {
+      newPeerConnection.onnegotiationneeded = async () => {
+        try {
+          newPeerConnection
+            .createOffer()
+            .then((offer) => {
+              newPeerConnection.setLocalDescription(offer);
+              console.log("Sending this OFFER to the requester:", offer);
+              HTMLBRElement.pushEvent("new_sdp_offer", {
+                toUser: fromUser,
+                description: offer,
+              });
+            })
+            .catch((err) => console.log(err));
+        } catch (e) {
+          console.log(e);
+        }
+      };
+    }
+
+    newPeerConnection.ontrack = async (event) => {
+      console.log("Track received:", event);
+      document.getElementById(fromUser).srcObject = event.streams[0];
+    };
+
+    return newPeerConnection;
+  }
+}
+
+Hooks.HandleOfferRequest = {
+  mounted() {
+    console.log("new offer request from", this.el.dataset.fromUserUuid)
+    let fromUser = this.el.dataset.fromUserUuid
+    createPeerConnection(this, fromUser)
+  }
+}
+
+Hooks.HandleIceCandidateOffer = {
+  mounted() {
+    let data = this.el.dataset
+    let fromUser = data.fromUser
+    let iceCandidate = JSON.parse(data.iceCandidate)
+    let peerConnection = users[fromUser].peerConnection
+
+    console.log("new ice candidate from", fromUser, iceCandidate)
+
+    peerConnection.addIceCandidate(iceCandidate)
+  }
+}
+
+Hooks.handleSdpOffer = {
+  mounted() {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+
+    if(sdp != "") {
+      console.log("new sdp OFFER from", data.fromUserUuid, data.sdp)
+
+      createPeerConnection(this, fromUser, sdp)
+    }
+  }
+}
+
+Hooks.HandleAnswer = {
+  mount() {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+    let peerConnection = users[fromUser].peerConnection
+
+    if(sdp != "") {
+      console.log("new sdp ANSWER from", fromUser, sdp)
+      peerConnection.setRemoteDescription({type: "answer", sdp: sdp})
+    }
+  }
+}
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.delayedShow(200))
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (info) =>
+  topbar.delayedShow(200)
+);
+window.addEventListener("phx:page-loading-stop", (info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
+liveSocket.enableDebug();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
--- lib/sameplace_web/live/room_live/show.ex
+++ lib/sameplace_web/live/room_live/show.ex
@@ -1,6 +1,7 @@
 defmodule SameplaceWeb.RoomLive.Show do
   use SameplaceWeb, :live_view
 
+  alias Phoenix.Socket.Broadcast
   alias Sameplace.Organizer
   alias Sameplace.ConnectedUser
   alias Sameplace.Presence
@@ -16,6 +17,7 @@ defmodule SameplaceWeb.RoomLive.Show do
     room = Organizer.get_room!(id)
     slug = room.slug
     Phoenix.PubSub.subscribe(Sameplace.PubSub, "room:#{slug}")
+    Phoenix.PubSub.subscribe(Sameplace.PubSub, "room:#{slug}:#{user.uuid}")
     {:ok, _} = Presence.track(self(), "room:" <> slug, user.uuid, %{})
 
     {:noreply,
@@ -24,7 +26,51 @@ defmodule SameplaceWeb.RoomLive.Show do
      |> assign(:room, Organizer.get_room!(id))
      |> assign(:user, user)
      |> assign(:slug, slug)
-     |> assign(:connected_users, [])}
+     |> assign(:connected_users, [])
+     |> assign(:offer_requests, [])
+     |> assign(:ice_candidate_offers, [])
+     |> assign(:sdp_offers, [])
+     |> assign(:answers, [])}
+  end
+
+  @impl true
+  def handle_event("new_ice_candidate", payload, socket) do
+    payload = Map.merge(payload, %{"from_user" => socket.assigns.user.uuid})
+
+    send_direct_message(socket.assigns.slug, payload["toUser"], "new_ice_candidate", payload)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("new_sdp_offer", payload, socket) do
+    payload = Map.merge(payload, %{"from_user" => socket.assigns.user.uuid})
+
+    send_direct_message(socket.assigns.slug, payload["toUser"], "new_sdp_offer", payload)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("new_answer", payload, socket) do
+    payload = Map.merge(payload, %{"from_user" => socket.assigns.user.uuid})
+
+    send_direct_message(socket.assigns.slug, payload["toUser"], "new_answer", payload)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("join_call", _params, socket) do
+    for user <- socket.assigns.connected_users do
+      send_direct_message(
+        socket.assigns.slug,
+        user,
+        "request_offers",
+        %{
+          from_user: socket.assigns.user
+        }
+      )
+    end
+
+    {:noreply, socket}
   end
 
   @impl true
@@ -34,6 +80,34 @@ defmodule SameplaceWeb.RoomLive.Show do
     {:noreply,
      socket
      |> assign(:connected_users, list_present(socket))}
+  end
+
+  @impl true
+  def handle_info(%Broadcast{event: "request_offers", payload: request}, socket) do
+    {:noreply,
+     socket
+     |> assign(:offer_requests, socket.assigns.offer_requests ++ [request])}
+  end
+
+  @impl true
+  def handle_info(%Broadcast{event: "new_ice_candidate", payload: payload}, socket) do
+    {:noreply,
+     socket
+     |> assign(:ice_candidate_offers, socket.assigns.ice_candidate_offers ++ [payload])}
+  end
+
+  @impl true
+  def handle_info(%Broadcast{event: "new_sdp_offer", payload: payload}, socket) do
+    {:noreply,
+     socket
+     |> assign(:sdp_offers, socket.assigns.ice_candidate_offers ++ [payload])}
+  end
+
+  @impl true
+  def handle_info(%Broadcast{event: "new_answer", payload: payload}, socket) do
+    {:noreply,
+     socket
+     |> assign(:answers, socket.assigns.answers ++ [payload])}
   end
 
   defp list_present(socket) do
@@ -48,5 +122,14 @@ defmodule SameplaceWeb.RoomLive.Show do
 
   defp create_connected_user do
     %ConnectedUser{uuid: Ecto.UUID.generate()}
+  end
+
+  defp send_direct_message(slug, to_user, event, payload) do
+    SameplaceWeb.Endpoint.broadcast_from(
+      self(),
+      "room:" <> slug <> ":" <> to_user,
+      event,
+      payload
+    )
   end
 end
